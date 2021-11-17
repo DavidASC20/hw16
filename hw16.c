85b1c69a0ec66429aeddcedef3d0fb551f526f93
DavidASC20 <djchong24@gmail.com>
2021-11-16 22:47:29 -0500

    finished

---
+++ hw16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <time.h>
+
+int main(){
+    printf("Sup Mother Forker :)\n");
+    int forker = fork();
+    int forker2;
+    if(forker){
+        forker2 = fork();
+    }
+    if(forker && forker2){
+        int status;
+        int pid = wait(&status);
+        printf("forker %d finished, took %d seconds\n", pid, WEXITSTATUS);
+        return 0;
+    }
+
+    srand(getpid());
+    int sleep_time = (rand() % 4) + 2;
+    sleep(sleep_time);
+    printf("Forker finished sleepin for %d secs\n", sleep_time);
+    return sleep_time;
+}
---
+++ makefile
@@ -0,0 +1,5 @@
+all: hw16.o
+	gcc -o hw16 hw16.o
+
+hw16.o: hw16.c
+	gcc -c hw16.c
